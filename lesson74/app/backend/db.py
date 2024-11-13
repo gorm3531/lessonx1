a5f697d3c52c69e7011cc44245f7ecb47e15784e
gorm3531 <mcgermafrodit@gmail.com>
2024-11-13 17:44:13 +0700

    Add files via upload

---
+++ lesson74/app/backend/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
+from sqlalchemy.orm import relationship
+engine = create_engine('sqlite:///taskmanager.db', echo=True)
+
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
---
+++ lesson74/app/models/task.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from app.backend.db import Base
+from app.models import *
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=0)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    slug = Column(String, unique=True, index=True)
+    user = relationship('User', back_populates='tasks')
+
+
+#from sqlalchemy.schema import CreateTable
+
+#print(CreateTable(Task.__table__))
